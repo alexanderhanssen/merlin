aa831927c8e1417318ced82bb3e185f19a4e238b
Alexander Hanssen <alexhsn1@gmail.com>
2016-01-07 10:55:42 +0100

    Adds post endpoint to read data

---
+++ Merlin/Models/TrafficMeasurement.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Merlin.Models
+{
+    public class TrafficMeasurement
+    {
+        [JsonProperty("measure_point_number")]
+        public int MeasurePointNumber { get; set; }
+        public string TimeStamp { get; set; }
+        [JsonProperty("event_number")]
+        public int EventNumber { get; set; }
+        public float Speed { get; set; }
+        public int Lane { get; set; }
+    }
+}
