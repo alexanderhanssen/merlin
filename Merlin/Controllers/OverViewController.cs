5de4e5daebc0015059855036ac6d4ae9756df38e
Alexander Hanssen <alexhsn1@gmail.com>
2016-01-07 16:46:15 +0100

    Overview checks how many messages there are in couchdb

---
+++ Merlin/Controllers/OverViewController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Merlin.Models;
+using MyCouch;
+using MyCouch.Requests;
+
+namespace Merlin.Controllers
+{
+    public class OverviewController : Controller
+    {
+        // GET: OverView
+        public ActionResult Index()
+        {
+            long totalRows;
+            using (var client = new MyCouchClient("http://db-couchdb.cloudapp.net:5984", "bekk4"))
+            {
+                var toTime = DateTime.Now;
+                var fromTime = toTime.Subtract(new TimeSpan(0, 0, 10));
+                var query = new QueryViewRequest("test", "countall");
+                totalRows = client.Views.QueryAsync(query).Result.Rows.Select(x => long.Parse(x.Value)).First();
+            }
+            var viewModel = new OverviewViewModel()
+            {
+                MeasurementPoints = new List<int>() {1500201, 1500202},
+                TotalRows = totalRows
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Merlin/Models/OverviewViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Merlin.Models
+{
+    public class OverviewViewModel
+    {
+        public List<int> MeasurementPoints { get; set; }
+        public long TotalRows { get; set; }
+    }
+}
