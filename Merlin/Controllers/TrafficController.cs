b152997a8eeda7e56249f355b4da82f86ca45d34
Alexander Hanssen <alexhsn1@gmail.com>
2016-01-07 11:34:09 +0100

    Adds post to couch db

--- Merlin/Controllers/TrafficController.cs
+++ Merlin/Controllers/TrafficController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Merlin.Models;
+using MyCouch;
 
 namespace Merlin.Controllers
 {
@@ -23,8 +24,12 @@ namespace Merlin.Controllers
         }
 
         // POST api/<controller>
-        public void Post([FromBody]IEnumerable<TrafficMeasurement> trafficMeasurements)
+        public void Post([FromBody]object data)
         {
+            using (var client = new MyCouchClient("db-couchdb.cloudapp.net:5984","mydb"))
+            {
+                client.Documents.PostAsync(data.ToString());
+            }
         }
 
         // PUT api/<controller>/5
