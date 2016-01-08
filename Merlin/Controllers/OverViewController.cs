3ef07147f9a0705e2cfdda7c04f2ef3c6ad919a5
Alexander Hanssen <alexhsn1@gmail.com>
2016-01-08 11:12:24 +0100

    Shows average speed as well

--- Merlin/Controllers/OverViewController.cs
+++ Merlin/Controllers/OverViewController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
@@ -31,12 +32,21 @@ namespace Merlin.Controllers
             using (var client = new MyCouchClient("http://db-couchdb.cloudapp.net:5984", "bekk4"))
             {
                 var startTime = DateTime.Now.Subtract(new TimeSpan(0, minutesAgo, 0)).ToString("O");
-                var query = new QueryViewRequest("test", "count_cars").Configure(x => x.StartKey(new object [] {measurePoint, startTime}).EndKey(new object[] {measurePoint ,DateTime.Now.ToString("O")}));
-                var result = await client.Views.QueryAsync(query);
+                var startKey = new object[] {measurePoint, startTime};
+                var endKey = new object[] {measurePoint, DateTime.Now.ToString("O")};
+                var carCountQuery = new QueryViewRequest("stats", "count").Configure(x => x.StartKey(startKey).EndKey(endKey));
+                var carCountResult = await client.Views.QueryAsync(carCountQuery);
+                var averageSpeedQuery = new QueryViewRequest("stats", "speed").Configure(x => x.StartKey(startKey).EndKey(endKey));
+                var averageSpeedResult = await client.Views.QueryAsync(averageSpeedQuery);
                 viewModel.MeasurePoint = measurePoint;
-                var firstOrDefault = result.Rows.FirstOrDefault();
-                if (firstOrDefault != null)
-                    viewModel.NumberOfCars = int.Parse(firstOrDefault.Value);
+                var carCount = carCountResult.Rows.FirstOrDefault();
+                if (carCount != null)
+                    viewModel.NumberOfCars = int.Parse(carCount.Value);
+                var averageSpeed = averageSpeedResult.Rows.FirstOrDefault();
+                if (averageSpeed != null)
+                {
+                    viewModel.AverageSpeed = float.Parse(averageSpeed.Value, CultureInfo.InvariantCulture);
+                }
             }
 
             return PartialView(viewModel);
--- Merlin/Models/OverviewViewModel.cs
+++ Merlin/Models/OverviewViewModel.cs
@@ -5,7 +5,6 @@ namespace Merlin.Models
     public class OverviewViewModel
     {
         public IEnumerable<object> MeasurementPoints { get; set; }
-        public List<int> MinuteIntervals => new List<int> {1,5,30,60}; 
-        public long TotalRows { get; set; }
+        public List<int> MinuteIntervals => new List<int> {1,5,15,60}; 
     }
 }
